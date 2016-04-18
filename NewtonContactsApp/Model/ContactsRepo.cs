364d66136f460fc1e0590fcef88ddb2ea28827b0
Jonas Sternerson <jonas@sternerson.se>
2016-04-18 20:56:13 +0200

    started serialization as in ch. 6

--- NewtonContactsApp/Model/ContactsRepo.cs
+++ NewtonContactsApp/Model/ContactsRepo.cs
@@ -1,16 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 
 namespace NewtonContactsApp.Model
 {
     public class ContactsRepo : IContactsRepository
     {
-        public void Init()
-        {
+        private StorageFile file = null;
+        private List<Contact> contacts;
+        public bool IsInitialized { get { return null != file; } }
 
+        public ContactsRepo()
+        {
+            contacts = new List<Contact>();
+        }
+
+        public async Task InitializeAsync()
+        {
+            if (!IsInitialized)
+            {
+                await Initialize("contacts.xml");
+            }
+        }
+
+        private async Task Initialize(string filename)
+        {
+            DataContractSerializer serializer = new DataContractSerializer(typeof(List<Contact>));
+            file = await ApplicationData.Current.LocalFolder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
+            using (var stream = await file.OpenReadAsync())
+            {
+                serializer.ReadObject(stream.GetInputStreamAt(0).
+            }
         }
 
         public int Create(Contact contact)
